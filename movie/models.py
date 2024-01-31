8f2e642c294460549901cc3b76511fe7d2e4f478
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-01-31 12:17:47 +0530

    modified

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+	name=models.CharField(max_length=250)
+	desc=models.TextField()
+	image=models.ImageField()
+	movie_type=models.CharField(max_length=250)
+	release_date=models.DateField()
+	movie_lang=models.CharField(max_length=250)
+	class Meta:
+		verbose_name = "Movie"
+		verbose_name_plural = "Movies"
+	def __str__(self):
+		return self.name
+    
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
+def movie_list(request):
+	movies=Movie.objects.all()
+	return render(request,'movie.html',{'movies':movies})
